384e329ce49b25f1dc5c587dca7dfef0424da335
dmitrymail_k <dmitrymail_k@mail.ru>
2019-07-18 01:06:48 +0400

    Updates

---
+++ precache-manifest.718c5c60a7dff5e62ea8b240660e06e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cee46238694684939e0c",
+    "url": "/react-redux-boilerplate/static/js/runtime~main.b1f27c27.js"
+  },
+  {
+    "revision": "00fcde7c798e735deec8",
+    "url": "/react-redux-boilerplate/static/js/main.8033ca92.chunk.js"
+  },
+  {
+    "revision": "1c0c263b85efc5f080dc",
+    "url": "/react-redux-boilerplate/static/js/2.e96d2ade.chunk.js"
+  },
+  {
+    "revision": "00fcde7c798e735deec8",
+    "url": "/react-redux-boilerplate/static/css/main.7756a244.chunk.css"
+  },
+  {
+    "revision": "1c0c263b85efc5f080dc",
+    "url": "/react-redux-boilerplate/static/css/2.c416ce50.chunk.css"
+  },
+  {
+    "revision": "e7a3334227045f335710a61bda3ca5f0",
+    "url": "/react-redux-boilerplate/index.html"
+  }
+];
